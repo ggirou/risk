b9ca5240852b3a042382c65f739a774de45eec08
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2014-03-18 23:22:44 +0100

    center in json

--- web/board.dart
+++ web/board.dart
@@ -1,5 +1,4 @@
 import 'dart:html';
-import 'dart:svg';
 import 'dart:convert';
 
 @MirrorsUsed(targets: 'risk.map')
@@ -23,16 +22,5 @@ class RiskBoard extends PolymerElement {
 
   countryClick(Event e, var detail, Element target) {
     target.classes.toggle('selected');
-  }
-
-  String center(String svg) {
-    if (svgPaths == null) return '';
-    final PathElement path = new PathElement()..setAttribute('d', svg);
-    $['svg'].append(path);
-    final b = path.getBBox();
-    final x = (b.x + b.width / 2);
-    final y = (b.y + b.height / 2);
-    path.remove();
-    return "$x $y";
   }
 }
