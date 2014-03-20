0e7bd3b939d444e1f318e431ce40a949f13a29a9
Guillaume Girou <ext.ggirou@kyriba.com>
2014-03-20 12:04:24 +0100

    Improve WebSocket test page to be able to send events

--- web/ws.dart
+++ web/ws.dart
@@ -1,9 +1,17 @@
 import 'dart:html';
   
+Element logs = querySelector("#logs");
+InputElement sendButton = querySelector("#sendButton");
+TextAreaElement eventInput = querySelector("#eventInput");
+
+const url = "ws://127.0.0.1:8080/ws";
+final ws = new WebSocket(url);
 
 main(){
-  print("Run");
-  final url = "ws://127.0.0.1:8080/ws";
-  final ws = new WebSocket(url);
-  ws.onMessage.listen((MessageEvent e) => print("Receive=${e.data}"));
+  ws.onMessage.listen(printEvent);
+  sendButton.onClick.listen((_) => ws.send(eventInput.value));
+}
+
+printEvent(MessageEvent event) {
+  logs.children.add(new LIElement()..text = event.data);
 }
