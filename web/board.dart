c35c7bac3109d0466079ba8915b8cd395753237e
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2014-03-20 23:04:54 +0100

    display armies and owner on map

--- web/board.dart
+++ web/board.dart
@@ -1,16 +1,20 @@
 import 'dart:html';
 import 'dart:convert';
 
-@MirrorsUsed(targets: 'risk.map')
+@MirrorsUsed(targets: const ['risk.map', 'risk.game',])
 import 'dart:mirrors';
 
 import 'package:polymer/polymer.dart';
+import 'package:risk/game.dart';
 import 'package:risk/map.dart';
 
+final COLORS = ['yellow', 'blue', 'green', 'brown', 'purple'];
 
 @CustomTag('risk-board')
 class RiskBoard extends PolymerElement {
   final List<Country> countries = COUNTRIES;
+
+  final game = new RiskGame(countries:FAKE_COUNTRY_STATES);
 
   @observable
   var svgPaths;
@@ -23,4 +27,51 @@ class RiskBoard extends PolymerElement {
   countryClick(Event e, var detail, Element target) {
     target.classes.toggle('selected');
   }
+
+  String color(int playerId) => COLORS[playerId % COLORS.length];
 }
+
+final FAKE_COUNTRY_STATES = {
+  'eastern_australia': new CountryState(1, 2),
+  'indonesia': new CountryState(0, 2),
+  'new_guinea': new CountryState(4, 4),
+  'alaska': new CountryState(2, 2),
+  'ontario': new CountryState(2, 3),
+  'northwest_territory': new CountryState(0, 2),
+  'venezuela': new CountryState(3, 2),
+  'madagascar': new CountryState(2, 2),
+  'north_africa': new CountryState(3, 3),
+  'greenland': new CountryState(4, 2),
+  'iceland': new CountryState(0, 4),
+  'great_britain': new CountryState(3, 1),
+  'scandinavia': new CountryState(4, 3),
+  'japan': new CountryState(3, 2),
+  'yakursk': new CountryState(1, 2),
+  'kamchatka': new CountryState(3, 1),
+  'siberia': new CountryState(3, 4),
+  'ural': new CountryState(3, 2),
+  'afghanistan': new CountryState(2, 4),
+  'middle_east': new CountryState(2, 3),
+  'india': new CountryState(2, 4),
+  'siam': new CountryState(1, 4),
+  'china': new CountryState(4, 1),
+  'mongolia': new CountryState(4, 1),
+  'irkutsk': new CountryState(1, 3),
+  'ukraine': new CountryState(4, 1),
+  'southern_europe': new CountryState(4, 2),
+  'western_europe': new CountryState(2, 3),
+  'northern_europe': new CountryState(1, 1),
+  'egypt': new CountryState(4, 1),
+  'east_africa': new CountryState(1, 3),
+  'congo': new CountryState(4, 4),
+  'south_africa': new CountryState(1, 1),
+  'brazil': new CountryState(1, 4),
+  'argentina': new CountryState(1, 4),
+  'eastern_united_states': new CountryState(3, 1),
+  'western_united_states': new CountryState(4, 2),
+  'quebec': new CountryState(1, 1),
+  'central_america': new CountryState(4, 1),
+  'peru': new CountryState(4, 2),
+  'western_australia': new CountryState(0, 1),
+  'alberta': new CountryState(1, 3),
+};
