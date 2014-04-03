f7a714f997cd94ad7a6ba1acbc6824084d5d1abf
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2014-04-03 21:11:15 +0200

    simplify StringToInt transformer

--- lib/polymer_transformer.dart
+++ lib/polymer_transformer.dart
@@ -1,8 +1,6 @@
 import 'package:polymer_expressions/filter.dart' show Transformer;
 
 class StringToInt extends Transformer<String, int> {
-  final int radix;
-  StringToInt({this.radix: 10});
   String forward(int i) => '$i';
-  int reverse(String s) => s == null ? null : int.parse(s, radix: radix, onError: (s) => null);
+  int reverse(String s) => int.parse(s);
 }
