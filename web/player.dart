671a113b925841fa44ad10413ef48923c132c030
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2014-04-08 10:33:00 +0200

    extract a component for player vignettes

---
+++ web/player.dart
@@ -0,0 +1,20 @@
+@MirrorsUsed(targets: const ['risk.game',])
+import 'dart:mirrors';
+
+import 'package:polymer/polymer.dart';
+import 'package:risk/game.dart';
+
+
+@CustomTag('risk-player-vignette')
+class RiskPlayerVignette extends PolymerElement {
+  // Whether styles from the document apply to the contents of the component
+  bool get applyAuthorStyles => true;
+
+  @published
+  PlayerState player;
+
+  @published
+  bool active;
+
+  RiskPlayerVignette.created(): super.created();
+}
