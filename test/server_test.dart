d03c03a4bebbd71395c0730005e8615d9cec6d35
mbreton <mbreton@xebia.fr>
2014-04-15 13:56:35 +0200

    Last try for UT

--- test/server_test.dart
+++ test/server_test.dart
@@ -31,19 +31,19 @@ testRiskWsServer() {
   group('', () {
     test('should add a player', () {
       // GIVEN
-      Stream stream = new Stream.fromIterable(['{"event":"Welcome","data":{"playerId":1}}', //
+      Stream stream = (new Stream.fromIterable(['{"event":"Welcome","data":{"playerId":1}}', //
       '{"event":"JoinGame","data":{"playerId":1,"name":"mat","avatar":"mao-zedong.png","color":"#f7676d"}}', //
-      '{"event":"PlayerJoined","data":{"playerId":1,"name":"mat","avatar":"mao-zedong.png","color":"#f7676d"}}']);
+      '{"event":"PlayerJoined","data":{"playerId":1,"name":"mat","avatar":"mao-zedong.png","color":"#f7676d"}}']))//
+      .asBroadcastStream() ;
 
-      /*check() {
+      check() {
         // THEN
-        print("THEN -------------");
         engine.getLogs(callsTo('handle')).verify(happenedExactly(3));
       }
       stream.listen(null, onDone:expectAsync(check));
 
       // WHEN
-      wsServer.listen(stream, 1);*/
+      wsServer.listen(stream, 1);
     });
 
 
